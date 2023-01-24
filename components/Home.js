e78ae8c626483813cb73ebe3f02ce20cc4e0b4a5
Julien Deville <j.deville@protonmail.com>
2023-01-24 15:32:52 +0100

    reverted changes to placeholder

--- components/Home.js
+++ components/Home.js
@@ -90,7 +90,7 @@ function Home() {
       </div>
       <div className={styles.bodyContainer}>
         <div className={styles.articlesContainer}>
-        {<LoadingPlaceholder/>}          
+        {isLoading ? <LoadingPlaceholder/> : articles}          
         </div>
         <div>
           <div className={styles.mainTagContainer}>
